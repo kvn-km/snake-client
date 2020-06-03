177f42c37ec265a39e865dce53c67302dcc8687b
Kevin Kim <kevinfkim@gmail.com>
2020-06-03 23:12:51 +0000

    refactored to create separate client and play modules.

--- play.js
+++ play.js
@@ -1,27 +1,4 @@
-const net = require("net");
-const stdin = process.stdin;
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "135.23.222.148",
-    port: 50541
-  });
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("connected.");
-  });
-
-  conn.on('data', (data) => {
-    console.log(data.toString());
-  });
-
-  conn.on('end', () => {
-    console.log('disconnected from server');
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting...");
 
