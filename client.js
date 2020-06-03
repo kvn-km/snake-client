375f00588f66b50c9855d6c2fb7270a822717dde
Kevin Kim <kevinfkim@gmail.com>
2020-06-03 23:19:55 +0000

    send/received info, such as a 3 char name, upon connection.

--- client.js
+++ client.js
@@ -2,20 +2,34 @@ const net = require("net");
 
 
 const connect = function() {
+
   const conn = net.createConnection({
     host: "135.23.222.148",
     port: 50541
   });
+
   conn.setEncoding("utf8");
+
   conn.on("connect", () => {
     console.log("connected.");
+    conn.write('KVN');
   });
+
+  conn.on('connect', () => {
+    conn.write('Name: KVN');
+  });
+
   conn.on('data', (data) => {
-    console.log(data.toString());
+    console.log("from server:", data.toString());
   });
+
   conn.on('end', () => {
     console.log('disconnected from server');
   });
+
+
+
+
   return conn;
 };
 
