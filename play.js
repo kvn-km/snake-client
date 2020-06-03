bbabb19b656be1cc6389a25160e04c0f346fec23
Kevin Kim <kevinfkim@gmail.com>
2020-06-03 23:02:42 +0000

    got connected, printed a string from server when disconnected.

---
+++ play.js
@@ -0,0 +1,28 @@
+const net = require("net");
+const stdin = process.stdin;
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: "135.23.222.148",
+    port: 50541
+  });
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("connected.");
+  });
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  conn.on('end', () => {
+    console.log('disconnected from server');
+  });
+
+  return conn;
+};
+
+console.log("Connecting...");
+
+connect();
