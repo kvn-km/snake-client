11a2cefc8b5ad2a6d592b9f4c6ba347a119d8889
Kevin Kim <kevinfkim@gmail.com>
2020-06-04 01:00:51 +0000

    setup a stdin handleUserInput handler to register CTRL C, before we can start to register other keypresses.

--- play.js
+++ play.js
@@ -2,4 +2,22 @@ const { connect } = require("./client");
 
 console.log("Connecting...");
 
-connect();
+const setupInput = function(conn) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", (data) => {
+    handleUserInput(data);
+  });
+  return stdin;
+};
+
+let handleUserInput = function(data) {
+  if (data === '\u0003') { // ctrl+c = "\u0003"
+    process.exit();
+  }
+};
+
+setupInput(connect());
+// connect(); // moved connect call into setupInput
