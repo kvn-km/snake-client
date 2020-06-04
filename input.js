e378fbe58c5202f7da289f6853722a3b3ee30006
Kevin Kim <kevinfkim@gmail.com>
2020-06-04 01:57:01 +0000

    updated input module to accept WASD movement keys.

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+let connection;
+
 const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
@@ -6,13 +8,41 @@ const setupInput = function(conn) {
   stdin.on("data", (data) => {
     handleUserInput(data);
   });
+  connection = conn;
   return stdin;
 };
+
+// connection.write("Move: up");
 
 let handleUserInput = function(data) {
   if (data === '\u0003') { // ctrl+c = "\u0003"
     process.exit();
   }
+  if (data === "w") {
+    connection.write("Move: up");
+    connection.write("Move: up");
+  }
+  if (data === "s") {
+    connection.write("Move: down");
+    connection.write("Move: down");
+  }
+  if (data === "a") {
+    connection.write("Move: left");
+    connection.write("Move: left");
+  }
+  if (data === "d") {
+    connection.write("Move: right");
+    connection.write("Move: right");
+  }
+
+
+
+
+
+
 };
+
+
+
 
 module.exports = { setupInput };
